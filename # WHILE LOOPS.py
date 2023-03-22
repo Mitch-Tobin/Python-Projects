5818b17f21846e0e0b350934406ebc08c18044ba
Mitchell Tobin <94295478+Mitch-Tobin@users.noreply.github.com>
2023-03-22 13:38:12 -0400

    Add files via upload
    Explaining While loops - After School Program

---
+++ # WHILE LOOPS.py
@@ -0,0 +1,29 @@
+# WHILE LOOPS
+
+# A While loop will execute a set of statements as long as a condition is true
+# Think of video games, as long as our video game is {on} / playing the code will
+# run allowing the video game to be played
+
+# Example 1 - Easy while loop
+i = 1
+while i < 6:
+    print(i)  # print the value of i
+    i+=1      # add one to i
+
+# This code will run continuously until our i variable is at the number 6
+# Notice the formatting is similar to our if/else statements with the colon : and our indentation
+
+# We can actually even use ELSE Staments with our while loops
+
+# ELSE STATEMENT WITH WHILE LOOP EXAMPLE
+i2 = 1
+while i2 < 6:
+    print(i2)
+    i2+=1
+else:
+    print("i2 is no longer less than 6")
+
+# This code will print a condition once i2 is no longer less than 6
+
+
+
